f9e8fa7ce0da0f2722291031a617c48cebd92e32
bhernv7 <hernandezv.bryan@gmail.com>
2025-03-17 05:57:02 -0600

    Se incluye funcion para "Listar Amigos"

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
 
 function agregarAmigo() {
-    if (newName = document.getElementById("amigo").value == "") {
-        alert("Por favor, inserte un nombre.")
+    if (document.getElementById("amigo").value == "") {
+        alert("Por favor, inserte un nombre.");
     } else {
-        amigos.push(newName)
+        let newName = document.getElementById("amigo").value
+        amigos.push(newName);
         //call listar participantes
-        document.getElementById("amigo").value = ''
+        console.log(amigos)
+        listarAmigos ();
+        document.getElementById("amigo").value = '';
     }
     
+}
+
+function listarAmigos(){
+    document.getElementById("listaAmigos").innerHTML = "";
+    let totalItems = amigos.length;
+    for (i=0; i < totalItems; i++) {
+        document.getElementById("listaAmigos").innerHTML += `<ul>${amigos[i]}</ul>`;
+    }
 }
