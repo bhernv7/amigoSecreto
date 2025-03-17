844b5bc0433526f5599e2060334f7707bcea1003
bhernv7 <hernandezv.bryan@gmail.com>
2025-03-17 05:14:38 -0600

    Se incluye lista y funcion de array (Agregar amigo).

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+
+function agregarAmigo() {
+    if (newName = document.getElementById("amigo").value == "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(newName)
+        //call listar participantes
+        document.getElementById("amigo").value = ''
+    }
+    
+}
